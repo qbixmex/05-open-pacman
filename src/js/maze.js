3934a5fe2c9fda832d5784884cef7f280b2709ed
Daniel González <qbixmex@gmail.com>
2026-09-21 12:25:02 -0600

    Implemented step 1, four ghosts

--- src/js/maze.js
+++ src/js/maze.js
@@ -52,8 +52,10 @@ const MAZE = MAZE_STR.map( ( row ) => row.split( '' ).map( parseTile ) );
 const TUNNEL_ROW = 14;
 const PACMAN_START = { x: 13, y: 23 };
 const GHOST_STARTS = [
-  { x: 13, y: 14, kind: 'hunter' }, // inside the pen
-  { x: 14, y: 14, kind: 'random' }, // inside the pen
+  { x: 13, y: 14, kind: 'blinky' },
+  { x: 14, y: 14, kind: 'pinky' },
+  { x: 13, y: 15, kind: 'inkey' },
+  { x: 14, y: 15, kind: 'clyde' },
 ];
 
 window.MAZE = MAZE;
